ef5b4c76e6c2dfaa7b517968cb76fadc10c0293c
Muhammad Fakhryan <fakhryandev@gmail.com>
2023-04-05 23:59:52 +0700

    fix directory timestamp-microservice

---
+++ timestamp-microservice/index.js
@@ -0,0 +1,70 @@
+// index.js
+// where your node app starts
+
+// init project
+require("dotenv").config();
+var express = require("express");
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// your first API endpoint...
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
+});
+
+app.get("/api/:date?", function (req, res) {
+  const date = req.params.date;
+
+  if (date) {
+    if (Number.isInteger(Number(date))) {
+      const unixTimestamp = parseInt(date);
+      const unixToDate = new Date(unixTimestamp);
+      const utc = unixToDate.toUTCString();
+
+      return res.json({
+        unix: unixTimestamp,
+        utc,
+      });
+    }
+
+    const timeStamp = Date.parse(date);
+    if (isNaN(timeStamp)) {
+      return res.json({
+        error: "Invalid Date",
+      });
+    }
+
+    const unixTimestamp = new Date(timeStamp).getTime();
+    const utc = new Date(timeStamp).toUTCString();
+
+    return res.json({
+      unix: unixTimestamp,
+      utc,
+    });
+  } else {
+    const unixTimestamp = new Date().getTime();
+    const utc = new Date().toUTCString();
+
+    return res.json({
+      unix: unixTimestamp,
+      utc,
+    });
+  }
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
