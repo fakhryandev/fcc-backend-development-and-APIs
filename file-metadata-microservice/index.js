6456003c769f1927268b5b18a3c6c17f264fd6f5
Muhammad Fakhryan <fakhryandev@gmail.com>
2023-04-16 23:52:27 +0700

    add file metadata microservice solution

---
+++ file-metadata-microservice/index.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var cors = require("cors");
+const multer = require("multer");
+require("dotenv").config();
+
+const upload = multer();
+
+var app = express();
+
+app.use(cors());
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  const { file } = req;
+
+  if (!file) {
+    return res.json({
+      error_message: "request invalid",
+    });
+  }
+
+  return res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size,
+  });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Your app is listening on port " + port);
+});
