033ee9a126d54a217958b4a22d049da171ebb717
Muhammad Fakhryan <fakhryandev@gmail.com>
2023-04-19 23:57:59 +0700

    feat: log exercise

--- exercise-tracker/controllers/ExerciseController.js
+++ exercise-tracker/controllers/ExerciseController.js
@@ -1,30 +1,53 @@
+const moment = require("moment");
 const Exercise = require("../models/Exercise");
+const User = require("../models/User");
 
 exports.create_exercise = (req, res) => {
   const userID = req.params.userID;
 
   const { description, duration, date } = req.body;
 
-  const exercise = new Exercise({
-    description,
-    duration,
-    date,
-    userID,
-  });
+  User.findById(userID)
+    .then((user) => {
+      const exercise = new Exercise({
+        description,
+        duration,
+        date: date ? date : new Date(),
+        userID: user.userID,
+      });
 
-  exercise
-    .save()
+      exercise.save();
+    })
     .then((data) => {
-      res.json({
-        username: data.userID,
+      return res.json({
+        // username: user.username,
         description: data.description,
         duration: data.duration,
-        date: data.date,
+        date: moment(data.date).format("DDD MM DD YYYY"),
         _id: data.id,
       });
     })
     .catch((err) => {
-      console.log(err);
+      res.json({
+        message: err._message,
+      });
+    });
+};
+
+exports.get_exercises = (req, res) => {
+  const userID = req.params.userID;
+
+  User.findById(userID)
+    .populate("exercise")
+    .exec()
+    .then((data) => {
+      console.log("---");
+      console.log(data);
+      // return res.json({
+      //   username: data.
+      // })
+    })
+    .catch((err) => {
       res.json({
         message: err._message,
       });
--- exercise-tracker/routes/api.js
+++ exercise-tracker/routes/api.js
@@ -7,5 +7,6 @@ const exerciseController = require("../controllers/ExerciseController");
 router.post("/users", userController.create_user);
 router.get("/users", userController.get_users);
 router.post("/users/:userID/exercises", exerciseController.create_exercise);
+router.get("/users/:userID/logs", exerciseController.get_exercises);
 
 module.exports = router;
